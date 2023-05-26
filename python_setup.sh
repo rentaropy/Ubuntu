4eb0fe414b993b1dd1994108d9b201a4fed4f8a5
maeda-doctoral <129837139+maeda-doctoral@users.noreply.github.com>
2023-05-26 22:33:00 +0900

    Update python_setup.sh

--- python_setup.sh
+++ python_setup.sh
@@ -10,3 +10,5 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.9 0
 sudo apt-get install -y python3.9-dev python3-pip python3.9-distutils
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.9
 python -m pip install --upgrade pip
+sudo apt install -y mysql-client libmysqlclient-dev libpango-1.0-0 libpangoft2-1.0-0
+python -m pip install -r requirements.txt

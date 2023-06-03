0e2b2ccb1b737b09b78c88e8f93cc02e2def2612
maeda-doctoral <129837139+maeda-doctoral@users.noreply.github.com>
2023-06-03 18:00:06 +0900

    Update python_setup.sh

--- python_setup.sh
+++ python_setup.sh
@@ -8,8 +8,8 @@ echo "" | sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install -y python3.9
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.9 0
 
-sudo apt-get install libreadline-dev zlib1g-dev libncursesw5-dev libssl-dev libsqlite3-dev  libgdbm-dev libc6-dev libbz2-dev
-sudo apt-get install build-essential libssl-dev libffi-dev  libexpat1-dev liblzma-dev
+sudo apt-get install -y libreadline-dev zlib1g-dev libncursesw5-dev libssl-dev libsqlite3-dev  libgdbm-dev libc6-dev libbz2-dev
+sudo apt-get install -y build-essential libssl-dev libffi-dev  libexpat1-dev liblzma-dev
 
 sudo apt-get install -y python3.9-dev python3-pip python3.9-distutils
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.9

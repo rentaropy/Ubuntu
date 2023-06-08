a8782e827afb592c49963a9c10aa9505dba1c20a
maeda-doctoral <129837139+maeda-doctoral@users.noreply.github.com>
2023-06-08 17:52:23 +0900

    Update user_setup.sh

--- user_setup.sh
+++ user_setup.sh
@@ -102,7 +102,8 @@ systemctl restart sshd.service
 curl https://raw.githubusercontent.com/maeda-doctoral/ubuntu_setup/main/update.sh && nano ./update.sh && chmod u+x ./update.sh
 
 crontab -l > {tmpfile}
-echo "*/5 * * * * curl ${GITHUB_KEYS_URL} > /home/ubuntu/.ssh/authorized_keys && chmod 600 /home/ubuntu/.ssh/authorized_keys" >> {tmpfile}
+echo "*/5 * * * * curl ${GITHUB_KEYS_URL} > /home/ubuntu/.ssh/authorized_keys && chmod 600 /home/ubuntu/.ssh/authorized_keys
+* * */2 * * /home/ubuntu/update.sh" >> {tmpfile}
 crontab {tmpfile}
 rm {tmpfile}
 

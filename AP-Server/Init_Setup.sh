c580f5c46536d31b84127e40ea1fab3676869221
maeda-doctoral <129837139+maeda-doctoral@users.noreply.github.com>
2023-07-19 15:37:37 +0900

    Update Init_Setup.sh

--- AP-Server/Init_Setup.sh
+++ AP-Server/Init_Setup.sh
@@ -101,8 +101,8 @@ chown ubuntu:ubuntu /home/ubuntu/.ssh/authorized_keys
 chmod 600 /home/ubuntu/.ssh/authorized_keys
 systemctl restart sshd.service
 
-curl https://raw.githubusercontent.com/maeda-doctoral/Ubuntu/main/Update.sh > /home/ubuntu/Update.sh
-chmod u+x ./Update.sh
+#curl https://raw.githubusercontent.com/maeda-doctoral/Ubuntu/main/Update.sh > /home/ubuntu/Update.sh
+#chmod u+x ./Update.sh
 
 crontab -l > {tmpfile}
 echo "*/5 * * * * curl ${GITHUB_KEYS_URL} > /home/ubuntu/.ssh/authorized_keys && chown ubuntu:ubuntu /home/ubuntu/.ssh/authorized_keys && chmod 600 /home/ubuntu/.ssh/authorized_keys

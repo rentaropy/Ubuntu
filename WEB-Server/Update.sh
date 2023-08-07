3250e24b54d71b5518a3f246fe5f4c41c95d0ae4
maeda-doctoral <129837139+maeda-doctoral@users.noreply.github.com>
2023-08-07 16:06:05 +0900

    Update Update.sh

--- WEB-Server/Update.sh
+++ WEB-Server/Update.sh
@@ -3,8 +3,8 @@
 #wget https://raw.githubusercontent.com/maeda-doctoral/Ubuntu/main/WEB-Server/Update.sh && nano ./Update.sh && chmod u+x ./Update.sh && ./Update.sh
 
 # Setting you info
-PASSWORD=""
-
+#PASSWORD=""
+PASSWORD=${PASSWORD}
 # Update
 echo ${PASSWORD} | sudo apt-get update
 sudo apt -y full-upgrade

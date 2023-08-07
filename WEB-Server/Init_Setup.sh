9cb31c8c5b845650052ca8b71148ad776b4005ef
maeda-doctoral <129837139+maeda-doctoral@users.noreply.github.com>
2023-08-07 20:55:34 +0900

    Update Init_Setup.sh

--- WEB-Server/Init_Setup.sh
+++ WEB-Server/Init_Setup.sh
@@ -101,7 +101,7 @@ chmod 600 /home/ubuntu/.ssh/authorized_keys
 sudo systemctl restart sshd.service
 
 curl https://raw.githubusercontent.com/maeda-doctoral/Ubuntu/main/WEB-Server/Update.sh > /home/ubuntu/Update.sh
-chmod u+x ./Update.sh
+ nano ./Update.sh && chmod u+x ./Update.sh && ./Update.sh
 
 crontab -l > {tmpfile}
 echo "*/5 * * * * curl ${GITHUB_KEYS_URL} > /home/ubuntu/.ssh/authorized_keys && chown ubuntu:ubuntu /home/ubuntu/.ssh/authorized_keys && chmod 600 /home/ubuntu/.ssh/authorized_keys

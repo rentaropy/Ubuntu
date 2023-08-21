4069ff083217ad5b94d8e70e68fe045bdd197946
maeda-doctoral <129837139+maeda-doctoral@users.noreply.github.com>
2023-08-21 19:13:31 +0900

    Create Nginx_Setup.sh

---
+++ WEB-Server/Nginx_Setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#wget https://raw.githubusercontent.com/maeda-doctoral/Ubuntu/main/WEB-Server/Nginx_Setup.sh && nano ./Nginx_Setup.sh && chmod u+x ./Nginx_Setup.sh && ./Nginx_Setup.sh
+
+sudo apt -y install nginx
+sudo ufw allow 'Nginx Full'
+sudo systemctl enable nginx
+
+IPADDRESS=$(hostname -I | awk '{print $1}')
+
+sudo sh -c "echo 'upstream edogawa {
+    server 127.0.1.1:8000;
+    # You can add more servers here if you want to load balance across multiple instances.
+}
+
+server {
+    listen 80;
+    server_name ${IPADDRESS};
+
+    location / {
+        proxy_pass http://edogawa;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    }
+    location /static {
+       autoindex on;
+       alias ~/Child-Guidance/accounts/static/;
+
+    }
+
+    error_page 400 401 403 404 /404.html;
+    error_page 500 502 503 504 /500.html;
+
+    location = /404.html {
+        internal;
+        root /var/www/html;
+    }
+
+    location = /500.html {
+        internal;
+        root /var/www/html;
+    }
+
+}' > /etc/nginx/sites-available/edogawa"
+
+sudo systemctl restart nginx
+sudo systemctl status nginx

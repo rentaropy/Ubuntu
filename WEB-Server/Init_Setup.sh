76fde12cc5d822f797c9613e0c0bfc472f341e18
maeda-doctoral <129837139+maeda-doctoral@users.noreply.github.com>
2023-08-21 18:47:01 +0900

    Update Init_Setup.sh

--- WEB-Server/Init_Setup.sh
+++ WEB-Server/Init_Setup.sh
@@ -20,7 +20,7 @@ sudo timedatectl set-timezone Asia/Tokyo
 sudo echo 'ubuntu ALL=NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
 
 # Firewall Allow
-sudo ufw allow 22
+sudo ufw allow 'OpenSSH'
 echo 'y' | sudo ufw enable
 
 # SSH Setup

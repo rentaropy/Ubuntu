75c7d334943f818fe5181c82c550b17c3ebf99d8
maeda-doctoral <129837139+maeda-doctoral@users.noreply.github.com>
2023-09-09 01:56:44 +0900

    Update Python39_Setup.sh

--- Python39_Setup.sh
+++ Python39_Setup.sh
@@ -12,5 +12,3 @@ sudo apt-get install -y build-essential libssl-dev libffi-dev  libexpat1-dev lib
 sudo apt-get install -y python3.9-dev python3-pip python3.9-distutils
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.9
 pip install --upgrade pip
-
-echo "Python 3.9 installation complete"

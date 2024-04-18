71536fb55a5935bbe3c0855d674b72b28281ac79
maeda-doctoral <129837139+maeda-doctoral@users.noreply.github.com>
2024-04-18 16:24:48 +0900

    Update and rename Python39_Setup.sh to Python39_WEB.sh

--- WEB-Server/Python39_Setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#wget https://raw.githubusercontent.com/maeda-doctoral/Ubuntu/main/WEB-Server/Python39_Setup.sh && nano ./Python39_Setup.sh && chmod u+x ./Python39_Setup.sh && ./Python39_Setup.sh
-
-sudo apt-get install -y mysql-client libmysqlclient-dev libpango-1.0-0 libpangoft2-1.0-0
---
+++ WEB-Server/Python39_WEB.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#wget https://raw.githubusercontent.com/maeda-doctoral/Ubuntu/main/WEB-Server/Python39_WEB.sh && nano ./Python39_WEB.sh && chmod u+x ./Python39_WEB.sh && ./Python39_WEB.sh
+
+sudo apt-get install -y mysql-client libmysqlclient-dev libpango-1.0-0 libpangoft2-1.0-0

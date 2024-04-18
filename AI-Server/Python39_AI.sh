e0115b91cab0d0f4eecad55c7cdcd0691a9b5aa7
maeda-doctoral <129837139+maeda-doctoral@users.noreply.github.com>
2024-04-18 21:38:30 +0900

    Update and rename Python39_DB.sh to Python39_AI.sh

---
+++ AI-Server/Python39_AI.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#wget https://raw.githubusercontent.com/maeda-doctoral/Ubuntu/main/AI-Server/Python39_AI.sh && nano ./Python39_AI.sh && chmod u+x ./Python39_AI.sh && ./Python39_AI.sh
+
+sudo apt-get install -y mysql-server libpango-1.0-0 libpangoft2-1.0-0
--- AI-Server/Python39_DB.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#wget https://raw.githubusercontent.com/maeda-doctoral/Ubuntu/main/DB-Server/Python39_DB.sh && nano ./Python39_DB.sh && chmod u+x ./Python39_DB.sh && ./Python39_DB.sh
-
-sudo apt-get install -y mysql-server libpango-1.0-0 libpangoft2-1.0-0

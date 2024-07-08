15cb432c1b4c592826ae5784f393b6d6b1641186
maeda-doctoral <129837139+maeda-doctoral@users.noreply.github.com>
2024-07-09 01:14:48 +0900

    Update Python312_Setup.sh

--- Python312_Setup.sh
+++ Python312_Setup.sh
@@ -11,7 +11,6 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.12 0
 sudo apt-get install -y libreadline-dev zlib1g-dev libncursesw5-dev libssl-dev libsqlite3-dev  libgdbm-dev libc6-dev libbz2-dev
 sudo apt-get install -y build-essential libssl-dev libffi-dev  libexpat1-dev liblzma-dev python3-testresources
 
-sudo apt-get install -y python3.12-dev python3-pip python3.12-distutils
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.12
 pip install --upgrade pip
 

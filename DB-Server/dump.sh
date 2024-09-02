7813b0081223fb60ac0513b55d140ac61abe6514
rentaropy <116675528+rentaropy@users.noreply.github.com>
2024-09-02 15:51:17 +0900

    Create dump.sh

---
+++ DB-Server/dump.sh
@@ -0,0 +1,11 @@
+#wget 
+
+mysql -u edogawa_user < $HOME/db_name.sql| grep -v "TABLE_SCHEMA" > $HOME/dump.log
+awk '$0="file_"$0"=$HOME/"$0"_dump_$(date +%Y%m%d).sql"' $HOME/dump.log >  $HOME/dump.exe
+awk '$0="mysqldump -u edogawa_user "$0" > $file_"$0  ' $HOME/dump.log >>  $HOME/dump.exe
+awk '$0="create database if not exists "$0";"' $HOME/dump.log   >  $HOME/create_db.sql
+chmod 700  $HOME/dump.exe
+$HOME/dump.exe
+find $HOME/edogawa*.sql  -mtime +3 -exec rm {} \;
+file1=$HOME/edogawa*.sql
+scp $file1 ubuntu@10.20.1.106:/home/ubuntu

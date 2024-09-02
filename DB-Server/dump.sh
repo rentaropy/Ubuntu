a53ba81db84ad1ecfb9d69a939f2a9f3d96310da
rentaropy <116675528+rentaropy@users.noreply.github.com>
2024-09-02 15:52:48 +0900

    Update dump.sh

--- DB-Server/dump.sh
+++ DB-Server/dump.sh
@@ -1,4 +1,4 @@
-#wget 
+#wget https://raw.githubusercontent.com/rentaropy/Ubuntu/main/DB-Server/dump.sh && nano ./dump.sh && chmod u+x ./dump.sh
 
 mysql -u edogawa_user < $HOME/db_name.sql| grep -v "TABLE_SCHEMA" > $HOME/dump.log
 awk '$0="file_"$0"=$HOME/"$0"_dump_$(date +%Y%m%d).sql"' $HOME/dump.log >  $HOME/dump.exe
